80d6c240da7c9ac24f0983ced4f2380b75f4018b
Arthur S Silveira <arthur@fooji.com>
2018-09-20 23:16:13 -0400

    trying to fix forms

--- src/components/pages/claim/Claim.js
+++ src/components/pages/claim/Claim.js
@@ -7,15 +7,6 @@ const About = () => (
       <div className='section-title'>Submit a claim</div>
       <div className='section-subtitle'>Online claims coming soon</div>
       <div className='mid-section claim-page'>
-        {/*<form className='contact-form' name='contact-form' method='POST' action='/thanks' data-netlify='true' data-netlify-honeypot='bot-field'>*/}
-          {/*<input type='hidden' name='form-name' value='contact-form' />*/}
-          {/*<input type='hidden' name='bot-field'/>*/}
-
-          {/*<input type='email' name='email' placeholder='Your email'/>*/}
-          {/*<input type='subject' name='subject' placeholder='Subject'/>*/}
-          {/*<textarea name='message' placeholder='How can I help you today?'/>*/}
-          {/*<button type='submit'>send</button>*/}
-        {/*</form>*/}
       </div>
     </div>
   </div>
--- src/components/pages/contact/Contact.js
+++ src/components/pages/contact/Contact.js
@@ -9,18 +9,19 @@ const Contact = () => (
       <div className='contact-split'>
         <div className='contact-left'>
           <div className='section-subtitle'> Just fill out this quick form and we will take care of the rest!</div>
-          <form className='contact-form' name='contact-form' method='POST' action='/thanks' data-netlify='true' data-netlify-honeypot='bot-field'>
-            <input type='hidden' name='form-name' value='contact-form' />
-            <input type='hidden' name='bot-field'/>
+
+          <form className='contact-form' name='contact' method='POST' data-netlify='true' data-netlify-honeypot='bot-field'>
+            <input type='hidden' name='form-name' value='contact' />
+            <input type='hidden' name='bot-field' />
 
             <input type='email' name='email' placeholder='Your email'/>
-            <input type='subject' name='subject' placeholder='Subject'/>
+            <input type='text' name='subject' placeholder='Subject'/>
             <textarea name='message' placeholder='How can we help you today?'/>
-            {/*<input type='file' name='subject' placeholder='Subject'/>*/}
             <div className='form-button-container'>
               <button className='trufit-button' type='submit'>send</button>
             </div>
           </form>
+
         </div>
         <div className='contact-right'>
           <div className='contact-map-container show-for-medium'>
